31d7c0029aa0e95286a322a24cdd152c6eaff065
Margaux Doisy <margauxdoisy@gmail.com>
2024-10-07 22:56:37 +0200

    logo footer

--- elizen/src/components/Footer.js
+++ elizen/src/components/Footer.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import '../styles/components/footer.css'
+import {Link} from "react-router-dom";
 
 const Footer = () => {
     return (
         <div className='footer'>
             <div className="logoFooter">
+                <Link to="/">
                 <img src="../asset/img/logo-beige-tr.png" alt="logo-beige-tr"/>
+                </Link>
             </div>
             <div>
                 <p>Elizen massages | Elise Debels</p>
 
-                <p><a href="../asset/mentions_légales.pdf" download="CGV">CGV</a></p>
-                <p><a href="#" download="#">Mentions légales</a></p>
+                <p><a href="../asset/CGV.pdf" download="CGV">CGV</a></p>
+                <p><a href="../asset/mentions_légales.pdf" download="Mentions_légales">Mentions légales</a></p>
                 <p><a href="../asset/cv.pdf" download="CV_Elise_Debels.pdf">Télécharger mon CV</a></p>
                 <p>massages.elizen@gmail.com</p>
                 <p>06 . 56 . 75 . 08 . 52</p>
